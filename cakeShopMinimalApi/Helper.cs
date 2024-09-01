9ebceaff2e0dc994d4a1e8a4bf376da23872b8e6
Milan Kaur <mikaur@microsoft.com>
2024-09-01 10:17:23 -0700

    Add project files.

---
+++ cakeShopMinimalApi/Helper.cs
@@ -0,0 +1,48 @@
+﻿namespace cakeShopMinimalApi
+{
+    using Azure.AI.OpenAI;
+    using Azure.AI.OpenAI.Chat;
+    using OpenAI.Chat;
+    using static System.Runtime.InteropServices.JavaScript.JSType;
+    using System.Security.Cryptography.Xml;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+
+    public static class Helper
+    {
+        public static string systemPrompt = "You are a cheerful and friendly AI bot designed to help callers place an order for a cake from Milan Cakeshop." +
+            " You have access to the cake flavors, sizes, and prices available in the Milan cake shop and your responses are grounded on that data. Refer to only that information. Do not hallucinate." +
+            " Your primary goal is to assist callers in selecting a cake flavor, size, and quantity and then telling them that their order is in the system." +
+            " You do not cite the document number in your responses" +
+            "Key Guidelines:" +
+            "Greeting: Start each interaction with a warm and friendly greeting." +
+            "Repetition: Do not repeat yourself unless the customer asks you to.Avoid unnecessary repetition in your responses."+
+            "Flavor Selection: Ask the caller if they have a specific flavor in mind."+
+            "If they’re unsure, help them by asking if they prefer a fruity, chocolaty, or another type of cake."+
+            "Provide only the names of the flavors that match their preference.If the caller is interested in a flavor, then and only then provide a detailed description."+
+            "Seasonal Flavors:When asked about seasonal flavors, provide only the names of the available seasonal cakes.If the caller expresses interest in a specific flavor, then describe that cake in detail."+
+            "Description: Once the caller selects a flavor, only then describe the cake in detail using the description from your knowledge base. Do not repeat the description" +
+            "Things you do not know : If a customer asks you a question for which you do not ahve an answer, politely say, sorry, I do not have that information, you will have to visit our website or the shop."+
+            "Random Question : If the customer talks about topics that are not realted to cake ordering, politely say sorry I can only help you place a cake order."+
+            "Size and Quantity:Ask the caller to choose the cake size (e.g., 6\" or 8\").\r\nConfirm the quantity they would like to order." +
+            "Custom Orders:If the caller requests a custom order, politely inform them that custom orders are not available through the bot and direct them to visit the shop for personalized service." +
+            "Placing an order:Ask the date of pickup. After confirming the order details, say: \"Thank you! Your order is in the system. Please complete the payment on our website to confirm the order." +
+            "Closing: End the interaction with a friendly farewell, thanking the caller for their order." +
+            "" +
+            "Example Interaction:" +
+            "Caller: \"Hi, I’d like to order a cake." +
+            "Bot: \"Hello! Thank you for choosing Milan Cakeshop! Do you have a flavor in mind, or can I help you find something fruity, chocolaty, or maybe something else?\"" +
+            "Caller: \"I’m thinking of something fruity.\"" +
+            "Bot: \"We have berry blast and mago pistachio\"" +
+            "Caller: \"Mango Pistachio sounds interesting\"" +
+            "Bot: \"Great choice! It has velvety pictachio cream with chunks of mango. What size would you like? We have 6 and 8 inch for 40 and 50 dollars respectively.\""+
+            "Caller: \"I'll take 6 inches\"" +
+            "Bot: \"Sure, what will be your pick up date?\""+
+            "Caller: \"5th September\""+
+            "Bot: \"Execllent! I am placing your order for a 6 inch mango pistachio cake for 5th September. Can you confirm if the details are correct?\""+
+            "Caller: \"yes\""+
+            "Bot: \"Awesome, blossom. Yous order is now in our system. Please make a payment on our website to confirm your order. Goodbye for now\"";
+
+        public static string reminderprompt = "Reminder : You are an AI assitant for Milan cake shop and your goal is to place an order for a cake or cakes in the system for the customer. After placing the order, you let themknow it's in the system, thank them and say goodbye.";
+    }   
+}
---
+++ cakeShopMinimalApi/Program.cs
@@ -0,0 +1,216 @@
+﻿using Azure.AI.OpenAI;
+using Azure.AI.OpenAI.Chat;
+using Azure.Communication;
+using Azure.Communication.CallAutomation;
+using Azure.Messaging;
+using cakeShopMinimalApi;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI.Chat;
+using System.Collections.Concurrent;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text;
+using Azure.Messaging.EventGrid.SystemEvents;
+using Azure.Messaging.EventGrid;
+using System.Text.RegularExpressions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .AddEnvironmentVariables()
+    .Build()
+    ?? throw new InvalidOperationException("Configuration is not provided.");
+
+var aiDeploymentName = config["OPENAI_DEPLOYMENT_NAME"] ?? throw new InvalidOperationException("OPENAI_DEPLOYMENT_NAME is not provided.");
+var openAIAPIKey = config["OPENAI_KEY"] ?? throw new InvalidOperationException("OPENAI_KEY is not provided.");
+var openAIUrl = config["OPENAI_ENDPOINT"] ?? throw new InvalidOperationException("OPENAI_ENDPOINT is not provided.");
+
+var aiSearchKey = config["AI_SEARCH_KEY"] ?? throw new InvalidOperationException("AI_SEARCH_KEY is not provided.");
+var aiSearchEndpoint = config["AI_SEARCH_ENDPOINT"] ?? throw new InvalidOperationException("AI_SEARCH_ENDPOINT is not provided.");
+var aiSearchIndex = config["AI_SEARCH_INDEXNAME"] ?? throw new InvalidOperationException("AI_SEARCH_INDEXNAME is not provided.");
+
+var acsConnectionString = config["ACS_CONNECTION_STRING"] ?? throw new InvalidOperationException("ACS_CONNECTION_STRING is not provided.");
+var cognitiveServicesEndpoint = config["AZURE_COG_SERVICES_ENDPOINT"] ?? throw new InvalidOperationException("AZURE_COG_SERVICES_ENDPOINT");
+var callbackUriHostString = config["CALLBACK_URI"] ?? throw new InvalidOperationException("CALLBACK_URI is not provided.");
+
+PhoneNumberIdentifier caller = new(config["ACS_PHONE_NUMBER"] ?? throw new InvalidOperationException("ACS_PHONE_NUMBER is not provided."));
+var speechVoiceName = "en-US-JennyMultilingualV2Neural";
+
+// Get Azure Open AI chat client
+AzureOpenAIClient _aiClient = new AzureOpenAIClient(
+    new Uri(openAIUrl),
+    new System.ClientModel.ApiKeyCredential(openAIAPIKey)
+);
+
+ChatClient chatClient = _aiClient.GetChatClient(aiDeploymentName);
+
+// Setting up the AI search index that has the cake shop knowledgebase as options for chat completion API
+ChatCompletionOptions options = new();
+options.AddDataSource(new AzureSearchChatDataSource()
+{
+    Endpoint = new Uri(aiSearchEndpoint),
+    IndexName = aiSearchIndex,
+    Authentication = DataSourceAuthentication.FromApiKey(aiSearchKey)
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+ConcurrentDictionary<string, List<ChatMessage>> chatHistoryCache = new();
+var callAutomationClient = new CallAutomationClient(acsConnectionString);
+
+// Sentence end symbols for splitting the response into sentences.
+List<string> sentenceSaperators = new() { ".", "!", "?", ";", "。", "！", "？", "；", "\n" };
+
+// Handle incoming call
+app.MapPost("/api/event", async ([FromBody] EventGridEvent[] eventGridEvents) =>
+{
+    foreach (var eventGridEvent in eventGridEvents)
+    {
+        // Handle system events
+        if (eventGridEvent.TryGetSystemEventData(out object eventData))
+        {
+            // Handle the subscription validation event
+            if (eventData is SubscriptionValidationEventData subscriptionValidationEventData)
+            {
+                var responseData = new SubscriptionValidationResponse
+                {
+                    ValidationResponse = subscriptionValidationEventData.ValidationCode
+                };
+                return Results.Ok(responseData);
+            }
+            else if (eventGridEvent.EventType == "Microsoft.Communication.IncomingCall")
+            {
+                if (eventData is AcsIncomingCallEventData incomingCallEventData)
+                {
+                    var encodedCallerId = WebUtility.UrlEncode(incomingCallEventData.FromCommunicationIdentifier.PhoneNumber.Value);
+                    var contextId = Guid.NewGuid().ToString();
+                    var callbackUri = new Uri(new Uri(callbackUriHostString), $"/api/callbacks/{contextId}?callerId={encodedCallerId}");
+                    var options = new AnswerCallOptions(incomingCallEventData.IncomingCallContext, callbackUri)
+                    {
+                        CallIntelligenceOptions = new CallIntelligenceOptions() { CognitiveServicesEndpoint = new Uri(cognitiveServicesEndpoint) }
+                    };
+
+                    chatHistoryCache[contextId] = new List<ChatMessage>();
+                    AnswerCallResult answerCallResult = await callAutomationClient.AnswerCallAsync(options);
+
+                    return Results.Ok();
+                }
+            }
+        }
+    }
+    return Results.Ok();
+});
+
+//Handle call back event such as recognize the speech of the customer, or call connected. These events are sent by the Event grid you need to configure in the ACS resource.
+app.MapPost("/api/callbacks/{contextId}", async (CloudEvent[] cloudEvents, ILogger<Program> logger, [FromRoute] string contextId,
+    [Required] string callerId) => {
+        foreach (var cloudEvent in cloudEvents)
+        {
+            var parsedEvent = CallAutomationEventParser.Parse(cloudEvent);
+            logger.LogInformation($"{parsedEvent?.GetType().Name} parsedEvent received for call connection id: {parsedEvent?.CallConnectionId}");
+            var callConnection = callAutomationClient.GetCallConnection(parsedEvent.CallConnectionId);
+
+            if (parsedEvent is CallConnected)
+            {
+                chatHistoryCache[contextId].Add(new SystemChatMessage(Helper.systemPrompt));
+
+                var connectMessage = "Hi, Welcome to Milan Cake shop. Are you calling to place an order for a cake?";
+                chatHistoryCache[contextId].Add(new AssistantChatMessage(connectMessage));
+                await SayAndRecognizeAsync(callConnection.GetCallMedia(), new PhoneNumberIdentifier(callerId), connectMessage);
+            }
+
+            if (parsedEvent is RecognizeFailed recognizeFailed && MediaEventReasonCode.RecognizeInitialSilenceTimedOut.Equals(parsedEvent.ResultInformation.SubCode.Value.ToString()))
+            {
+                Console.WriteLine($"Recognize failed: {parsedEvent.ResultInformation}");
+                var noResponse = "I'm sorry, I didn't hear anything. Are you still with me?";
+                await SayAndRecognizeAsync(callConnection.GetCallMedia(), new PhoneNumberIdentifier(callerId), noResponse);
+                chatHistoryCache[contextId].Add(new AssistantChatMessage(noResponse));
+            }
+
+            // This event is generated when the speech is recorded by call automation service. In other words, when the user on the other end of the line has completed their sentence
+            if (parsedEvent is RecognizeCompleted recogEvent
+                && recogEvent.RecognizeResult is SpeechResult speech_result)
+            {
+                 chatHistoryCache[contextId].Add(new UserChatMessage(speech_result.Speech));
+                chatHistoryCache[contextId].Add(new UserChatMessage (Helper.reminderprompt));
+
+
+                // calling Azure Open AI to get a response for the user based on the conversation history, knowledgebase and the system prompt
+
+                StringBuilder gptBuffer = new();
+
+                await foreach (StreamingChatCompletionUpdate update in chatClient.CompleteChatStreamingAsync(chatHistoryCache[contextId], options))
+                {
+                    var message = update.ContentUpdate;
+                    foreach (var item in message)
+                    {
+                        if (string.IsNullOrEmpty(item.Text))
+                        {
+                            continue;
+                        }
+
+                         
+                        if (sentenceSaperators.Any(item.Text.Contains))
+                        {
+                            var sentence = Regex.Replace(gptBuffer.ToString().Trim(), @"\[doc\d+\]", string.Empty);
+                            if (!string.IsNullOrEmpty(sentence))
+                            {
+                                chatHistoryCache[contextId].Add(new AssistantChatMessage(sentence));
+                                await SayAsync(callConnection.GetCallMedia(), new PhoneNumberIdentifier(callerId), sentence);
+                                Console.WriteLine($"\t > streamed: '{sentence}'");
+                                gptBuffer.Clear();
+                            }
+                        }
+                    }
+                }
+                await SayAndRecognizeAsync(callConnection.GetCallMedia(), new PhoneNumberIdentifier(callerId), ".");
+            }
+        }
+    });
+
+
+
+app.Run();
+
+TextSource CreateTextSource(string response) => new TextSource(response) { VoiceName = speechVoiceName };
+
+// Convert the message to speech and play on the connected call
+async Task SayAsync(CallMedia callConnectionMedia, PhoneNumberIdentifier phoneId, string response)
+{
+    var responseTextSource = CreateTextSource(response);
+    var recognize_result = await callConnectionMedia.PlayToAllAsync(new PlayToAllOptions([responseTextSource]));
+}
+
+async Task SayAndRecognizeAsync(CallMedia callConnectionMedia, PhoneNumberIdentifier phoneId, string response)
+{
+    // creates the 
+    var responseTextSource = CreateTextSource(response);
+
+    var recognizeOptions =
+        new CallMediaRecognizeSpeechOptions(
+            targetParticipant: CommunicationIdentifier.FromRawId(phoneId.RawId))
+        {
+            Prompt = responseTextSource,
+            EndSilenceTimeout = TimeSpan.FromMilliseconds(500)
+        };
+
+    var recognize_result = await callConnectionMedia.StartRecognizingAsync(recognizeOptions);
+}
+
+
